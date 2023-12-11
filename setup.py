24763b1b44c58a759a44a421a2fe8f56159e84d1
FernandoMoralesM01 <94548980+FernandoMoralesM01@users.noreply.github.com>
2023-12-10 20:36:02 -0600

    Cambio de nombre de la libreria

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='viconpy',
+    name='iberoSignalPro',
     version='0.1',
     packages=find_packages(),
     author="Fernando Morales Magallon, Pablo Roca Mendoza"
