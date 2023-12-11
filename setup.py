2b35d21a1327e752a05f114d27c19324e858d5b0
FernandoMoralesM01 <94548980+FernandoMoralesM01@users.noreply.github.com>
2023-12-10 19:22:04 -0600

    Creacion del primer módulo de la libreria: Vicon.
    Se creo el módulo vicon el cual tiene un objeto Vicon para poder leer los archivos almacenados en .csv que genera la apliación Vicon Nexus.
    Esta clase contiene propiedades: segments, joints, devices, model outputs y devices, los cuales están guardados en la carpeta de vicon, la función es capaz deddistinguir si es que falta alguno de estas partes.
    Las señales guardades en Devices típicamente están muestreadas a 2000 sps y las demás a 100 sps (al igual que las cámaras de vicon).

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='viconpy',
+    version='0.1',
+    packages=find_packages(),
+    author="Fernando Morales Magallon, Pablo Roca Mendoza"
+    # Agrega otros metadatos como author, description, etc.
+)
