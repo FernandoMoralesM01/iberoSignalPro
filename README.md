7cdc425607a6e38db57e390a12dee1c79b49de19
FernandoMoralesM01 <94548980+FernandoMoralesM01@users.noreply.github.com>
2024-12-26 11:07:26 -0600

    Update README.md for improved clarity and installation instructions; modify PKG-INFO to reflect new authorship and update setup.py for version and requirements.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # iberoSignalPro
 
-Libreria desarrollada en la Univesridad Iberoamericana para procesar distintas señales e imágenes.
-La libreria contiene modulos de vicon, de image y de preprocesamento de señales
+**iberoSignalPro** is a library developed at Universidad Iberoamericana to process various types of signals and images.  
+The library includes modules for:
+
+- **vicon**: Tools for motion capture data processing.
+- **imagen**: Functions for image processing.
+- **preprocesa**: Utilities for preprocessing signals.
+
+## Installation
+
+To install the library directly from GitHub, use the following command:
+
+```bash
+pip install git+https://github.com/yourusername/your-repo.git
--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
 setup(
     name='iberoSignalPro',
-    version='0.1',
+    version='0.2',
+    description= "A package to process signals and images",
     packages=find_packages(),
-    author="Fernando Morales Magallon, Pablo Roca Mendoza"
-    # Agrega otros metadatos como author, description, etc.
+    author="Fernando Morales Magallon, Erik Rene Bojorges, Pablo Roca Mendoza",
+    url = "https://github.com/FernandoMoralesM01/EEG-Voluntary-Involuntary-Movements",
+    install_requires=requirements,
+    python_requires=">=3.10",
+    # Agrega otros metadatos como author, description, etc. 
 )
